ba468c8809c7ac32c272d9a40998c2ebfe9da2d8
karol-velasco <kjrv1023@gmail.com>
2026-08-24 10:07:46 -0500

    feat: Completar Reto 3: arreglos y objetos

--- practica-js.js
+++ practica-js.js
@@ -47,6 +47,8 @@ console.log("Promedio: " + resultadoPromedio);
 //La diferencia entre console.log y return
 //Console.log sirve para mostrar informacion en la consolo o terminal, mientras que return devulve un valor desde una funcion para utilizarla luego.
 
+// --------------------------------------------------------------------- //
+
 /*Reto 2: funciones flecha*/
 
 function aplicarIva(precio) {
@@ -73,3 +75,18 @@ console.log(esMayorEdad(19));
 
 const nombreCompleto = (nombre, apellido) => nombre + " " + apellido;
 console.log(nombreCompleto("Karol", "Velasco"));
+
+// --------------------------------------------------------------------- //
+/*Reto 3*/
+const productos = [
+{ id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Perifericos', stock: 5 },
+{ id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Perifericos', stock: 0 },
+{ id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Pantallas', stock: 3 },
+{ id: 4, nombre: 'Audifonos', precio: 85000, categoria: 'Perifericos', stock: 9 },
+{ id: 5, nombre: 'Laptop', precio: 2900000, categoria: 'Computadores', stock: 4 },
+{ id: 6, nombre: 'Celular', precio: 1650000, categoria: 'Móviles', stock: 6 },
+{ id: 7, nombre: 'Tablet', precio: 860000, categoria: 'Móviles', stock: 0 },
+{ id: 8, nombre: 'Reloj Inteligente', precio: 190000, categoria: 'Accesorios', stock: 4 },
+{ id: 9, nombre: 'Televisor', precio: 2650000, categoria: 'Pantallas', stock: 0 },
+{ id: 10, nombre: 'Impresora', precio: 530000, categoria: 'Oficina', stock: 3 }
+];
