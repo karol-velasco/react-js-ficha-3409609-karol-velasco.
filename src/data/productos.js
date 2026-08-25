8b3cb3e77216acd71eb275bb3e00bcf35d243522
karol-velasco <kjrv1023@gmail.com>
2026-08-25 08:23:49 -0500

    feat: mostrar productos con React

--- src/data/productos.js
+++ src/data/productos.js
@@ -1,6 +1,12 @@
 export const productos = [
     { id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Perifericos', stock: 5 },
     { id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Perifericos', stock: 0 },
-    { id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Pantallas', stock: 3 }
-    // Agrega los demás productos de tu ejercicio
+    { id: 3, nombre: 'Monitor,', precio: 650000, categoria: 'Pantallas', stock: 3 },
+    { id: 4, nombre: 'Audifonos', precio: 85000, categoria: 'Perifericos', stock: 9 },
+    { id: 5, nombre: 'Laptop', precio: 2900000, categoria: 'Computadores', stock: 4 },
+    { id: 6, nombre: 'Celular', precio: 1650000, categoria: 'Móviles', stock: 6 },
+    { id: 7, nombre: 'Tablet', precio: 860000, categoria: 'Móviles', stock: 0 },
+    { id: 8, nombre: 'Reloj Inteligente', precio: 190000, categoria: 'Accesorios', stock: 4 },
+    { id: 9, nombre: 'Televisor', precio: 2650000, categoria: 'Pantallas', stock: 0 },
+    { id: 10, nombre: 'Impresora', precio: 530000, categoria: 'Oficina', stock: 3 }
 ];
