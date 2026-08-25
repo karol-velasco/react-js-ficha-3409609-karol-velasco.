847a7b59d96e4f58fd36330a147f141683ec841c
karol-velasco <kjrv1023@gmail.com>
2026-08-25 08:12:54 -0500

    feat: Crear carpeta data y un archivo con los productos

---
+++ src/data/productos.js
@@ -0,0 +1,6 @@
+export const productos = [
+    { id: 1, nombre: 'Mouse', precio: 50000, categoria: 'Perifericos', stock: 5 },
+    { id: 2, nombre: 'Teclado', precio: 90000, categoria: 'Perifericos', stock: 0 },
+    { id: 3, nombre: 'Monitor', precio: 650000, categoria: 'Pantallas', stock: 3 }
+    // Agrega los demás productos de tu ejercicio
+];
